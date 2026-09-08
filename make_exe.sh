22cddc225c3031f6739b306dd4b25cf37dd665ec
magictoast1733 <magictoast1733@gmail.com>
2026-09-08 15:46:18 +0700

    Delete make_exe.sh

--- make_exe.sh
+++
@@ -1,8 +0,0 @@
-  GNU nano 8.7.1                                         make_exe.sh *                                                
-#!/bin/bash
-
-sudo source env/bin/activate
-
-pyinstaller --onefile --noconsole --name NetBadger main.py
-
-sudo chown ubi /dist/NetBadger
